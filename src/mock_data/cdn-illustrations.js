518ae84af5c36637a5f177491daf4e928707d69b
exphoenee <bozzay.viktor@gmail.com>
2026-05-08 18:53:40 +0200

    added mock data

---
+++ src/mock_data/cdn-illustrations.js
@@ -0,0 +1,17 @@
+export const cdnIllustrations = {
+  "about-factory": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175074/about-factory_essdws.jpg",
+  "about-research": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175074/about-research_qjwvdq.jpg",
+  "category-mining": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175081/category-mining_lgz6gr.jpg",
+  "category-combat": "https://res.cloudinary.com/djemtfagn/image/upload/v1776348601/category-combat_jg8lwo.jpg",
+  "category-commerce": "https://res.cloudinary.com/djemtfagn/image/upload/v1776348601/category-commerce_pmmwpt.jpg",
+  "category-research": "https://res.cloudinary.com/djemtfagn/image/upload/v1776348602/category-research_grarg0.jpg",
+  "icon-customization": "https://res.cloudinary.com/djemtfagn/image/upload/v1776348602/icon-customization_ufbasy.png",
+  "competitive-income": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175083/competitive-income_mggj6p.jpg",
+  "growth-opportunities": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175092/growth-opportunities_wai0b7.jpg",
+  "hero-spacecraft": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175092/hero-spacecraft_kjfsxw.jpg",
+  "hero-spacecraft_old": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175093/hero-spacecraft_old_ljufgt.jpg",
+  "icon-innovation": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175096/icon-innovation_ydychk.jpg",
+  "innovative-environment-old": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175098/innovative-environment-old_fws0fk.jpg",
+  "innovative-environment": "https://res.cloudinary.com/djemtfagn/image/upload/v1776175097/innovative-environment_ndijll.jpg",
+  "factory-video": "https://res.cloudinary.com/djemtfagn/video/upload/v1776333088/factory_forward_reverse_j1uh8t.mp4"
+};
---
+++ src/mock_data/jobs.js
@@ -0,0 +1,46 @@
+export const jobs = [
+  {
+    "id": 1,
+    "title": "Takarító",
+    "department": "Üzemeltetési Osztály",
+    "count": "270 pozíció",
+    "description": "Gyártósoraink és irodáink tisztaságának fenntartása. Műszakos munkarend, versenyképes fizetés és cafeteria csomag.",
+    "type": "Teljes munkaidő",
+    "location": "Pécs, Naprendszer",
+    "isFeatured": false
+  },
+  {
+    "id": 2,
+    "title": "Űrjármű Tervező Mérnök",
+    "department": "Kutatás-Fejlesztési Osztály",
+    "count": "1 pozíció",
+    "description": "Következő generációs űrjárművek tervezése és fejlesztése. Minimum 5 év tapasztalat űriparban, CAD és szimuláció szoftverek ismerete szükséges.",
+    "requirements": "Mérnöki diploma, Űrtechnológiai tapasztalat, Angol nyelvtudás",
+    "type": "Teljes munkaidő",
+    "location": "Mars Kolónia, Naprendszer",
+    "salary": "Kiemelt",
+    "isFeatured": true
+  },
+  {
+    "id": 3,
+    "title": "Programozó (4 órás)",
+    "department": "IT Osztály",
+    "count": "1 pozíció",
+    "description": "Űrhajó navigációs rendszerek és irányítóprogramok fejlesztése. Napi 4 órás munkavégzés rugalmas időbeosztással, home office lehetőség.",
+    "requirements": "Python, C++, JavaScript ismeretek, Verziókezelés (Git)",
+    "type": "Részmunkaidő (4 óra/nap)",
+    "location": "Távmunka / Pécs",
+    "isFeatured": false
+  },
+  {
+    "id": 4,
+    "title": "Konyhás néni",
+    "department": "Éttermi Szolgáltatások",
+    "count": "134 pozíció",
+    "description": "Munkatársaink ellátása finom, tápláló ételekkel a vállalati étkezdében. Tapasztalat előny, de nem feltétel. Családias munkahelyi légkör.",
+    "requirements": "Élelmiszeripari tapasztalat, Higiéniai bizonyítvány",
+    "type": "Teljes munkaidő",
+    "location": "Pécs, Naprendszer",
+    "isFeatured": false
+  }
+];
