928f8d9737a4332bc3c044df33efc3bd3d8f3ed4
exphoenee <bozzay.viktor@gmail.com>
2026-05-08 17:49:16 +0200

    it works now

--- src/components/ResearchSection/index.jsx
+++ src/components/ResearchSection/index.jsx
@@ -1,25 +1,25 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-import {ResearchList as ships} from "./ships";
+// import {ResearchList as ships} from "./ships";
 import  CommerceCard from "../CommerceCard";
 
-export default function CommerceShips() {
-  // const [ships, setShips] = useState([]);
+export default function ResearchShips() {
+  const [ships, setShips] = useState([]);
 
-  // useEffect(() => {
-  //   async function fetchShips() {
-  //     const res = await fetch("https://ecore-backend-production.up.railway.app/api/spacecrafts/type/commercial");
-  //     const data = await res.json();
-  //     setShips(data);
-  //   }
-  //   fetchShips();
-  // }, []);
+  useEffect(() => {
+    async function fetchShips() {
+      const res = await fetch("https://ecore-backend-production.up.railway.app/api/spacecrafts/type/research");
+      const data = await res.json();
+      setShips(data);
+    }
+    fetchShips();
+  }, []);
 
   return (
     <>
       <div className="product-header">
-        <h1>Kereskedelmi járművek</h1>
-        <p>Megbízható és hatékony szállítási megoldások az űrben</p>
+        <h1>Kutatási járművek</h1>
+        <p>Megbízható és hatékony kutatási megoldások az űrben</p>
       </div>
       <div className="container">
         <ul className="products-grid">
